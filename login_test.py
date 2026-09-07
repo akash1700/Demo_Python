c6ce0c72f75b42cf6df517df729f583efc371bd4
Akas <akashmollick86@gmail.com>
2026-09-07 22:41:39 +0530

    Updated login files

--- login_test.py
+++ login_test.py
@@ -2,3 +2,13 @@ print("Login test")
 
 print("Login test started")
 print("Login successful")
+
+username = "admin"
+password = "1234"
+
+if username == "admin" and password == "1234":
+    print("Login successful")
+else:
+    print("Login failed")
+
+print("Login test completed")    
