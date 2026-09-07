e2e8cde13253d52c600c0b52edb729295c44332d
Akas <akashmollick86@gmail.com>
2026-09-08 00:14:20 +0530

    added main.py for payment process

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Login test")
+
+print("Login test started")
+print("Login successful")
+
+username = "admin"
+password = "1234"
+
+if username == "admin" and password == "1234":
+    print("Login successful")
+else:
+    print("Login failed")
+
+print("Login test completed")
+print("Login feature testing started") # This line is added in login-feature1
+
+print("code from payment branch")
